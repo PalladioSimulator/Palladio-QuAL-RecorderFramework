a67b47689591bd483650fa72fb12235612a2bdb5
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-09-08 12:06:52 +0000

    Added package info
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/RecorderFramework/trunk@24480 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.recorderframework/src/org/palladiosimulator/recorderframework/config/package-info.java
+++ org.palladiosimulator.recorderframework/src/org/palladiosimulator/recorderframework/config/package-info.java
@@ -1,4 +1,6 @@
 /**
+ * Package for configuration objects of recorders as well as for the creation of such configuration objects.
+ * 
  * @author Sebastian Lehrig
  */
 package org.palladiosimulator.recorderframework.config;
