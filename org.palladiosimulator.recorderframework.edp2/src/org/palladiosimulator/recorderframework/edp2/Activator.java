51e9745438180ea7c81624161ba3961f984410e5
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2016-03-03 12:40:51 +0000

    Made constant public such that it can be reused.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/RecorderFramework/trunk@31274 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.recorderframework.edp2/src/org/palladiosimulator/recorderframework/edp2/Activator.java
+++ org.palladiosimulator.recorderframework.edp2/src/org/palladiosimulator/recorderframework/edp2/Activator.java
@@ -11,6 +11,9 @@ public class Activator extends Plugin {
     /** The plug-in ID */
     public static final String PLUGIN_ID = "org.palladiosimulator.recorderframework.edp2";
 
+    /** The Recorder name */
+    public static final String EDP2_ID = "Experiment Data Persistency & Presentation (EDP2)";
+
     /** The shared instance */
     private static Activator plugin;
 
@@ -21,13 +24,13 @@ public class Activator extends Plugin {
     }
 
     @Override
-    public void start(BundleContext context) throws Exception {
+    public void start(final BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
     }
 
     @Override
-    public void stop(BundleContext context) throws Exception {
+    public void stop(final BundleContext context) throws Exception {
         plugin = null;
         super.stop(context);
     }
